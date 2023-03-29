8a0d4e55a18b09c4dfc4b9b6f0dbf3d1541be8aa
Dias <raibekovdias@gmail.com>
2023-03-29 23:27:48 +0600

    lab7

--- lab7/circle/pov_circle.py
+++ lab7/circle/pov_circle.py
@@ -18,8 +18,8 @@ while True:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
+            pygame.quit()  # uninitialize all pygame modules
+            quit()         # interpreter exit
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT and x >= 50:
                 x -= 20
